8782228283488324cce1f463529bb805e4cd81d3
David <garzadavid26@yahoo.com>
2022-03-01 17:51:13 -0800

    basically done

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,2 +1,24 @@
-const sketchPad = document.querySelector('#sketch-pad');
+const sketchPad = document.querySelector('.sketch-container');
 
+function createGrid(gridNum) {
+    for (i=0; i<gridNum; i++) {
+        for (j=0; j<gridNum; j++) {
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('sketch-box');
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.background = 'black';
+            });
+            sketchPad.appendChild(gridBox);
+        }
+    }
+}
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', () => {
+    const allGrid = document.querySelectorAll('.sketch-box');
+    allGrid.forEach((grid) => {
+        grid.style.background = 'white';
+    });
+});
+
+createGrid(16);
